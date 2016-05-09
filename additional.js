ecbfbe0dcbd9ad84f19fae2bfdba277f010f1e4b
Matt Brennan <mattyb1000@gmail.com>
2016-05-09 21:12:05 +0100

    split and generic

--- additional.js
+++ additional.js
@@ -1,17 +1,16 @@
-var groupBy = require('lodash.groupby');
 var mapValues = require('lodash.mapvalues');
+var property = require('lodash.property');
 
 var apportion = require('./apportion');
 var list = require('./list');
 var sumResults = require('./sum-results');
 var groupedResults = require('./grouped-results');
 
-function sumAllResults(results) {
-	return results.map(r => mapValues(groupBy(r.results, 'party'), '[0]votes')).reduce(sumResults, {});
-}
-
-var votes = mapValues(groupedResults, sumAllResults);
-
-module.exports = mapValues(votes, (result, name) => {
-	return apportion(list[name], result);
+module.exports = mapValues(groupedResults, (result, name) => {
+	return apportion(list[name], sumResults(
+		result,
+		property('results'),
+		'party',
+		'votes'
+	));
 });
--- sum-results.js
+++ sum-results.js
@@ -1,6 +1,20 @@
 var mergeWith = require('lodash.mergewith');
+var mapValues = require('lodash.mapvalues');
+var groupBy = require('lodash.groupby');
 var add = require('lodash.add');
+var map = require('lodash.map');
+var some = require('lodash.some');
 
-module.exports = function sumResults(resultA, resultB) {
+function sumResults(resultA, resultB) {
+	if(some(resultB, isNaN)) throw new Error('nan');
 	return mergeWith(resultA, resultB, add);
 }
+
+module.exports = function sumAllResults(results, getResults, keyKey, valueKey) {
+	return map(results, r =>
+		mapValues(
+			groupBy(getResults(r), keyKey),
+			'[0]' + valueKey
+		)
+	).reduce(sumResults, {});
+};
